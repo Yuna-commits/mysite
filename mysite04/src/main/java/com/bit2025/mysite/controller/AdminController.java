04ea98078cf4fa76d59d0661bf12a16aaf97f317
Yuna-commits <ksm13563661@gmail.com>
2025-09-16 16:19:42 +0900

    admin controller 코드 수정

--- mysite04/src/main/java/com/bit2025/mysite/controller/AdminController.java
+++ mysite04/src/main/java/com/bit2025/mysite/controller/AdminController.java
@@ -1,5 +1,7 @@
 package com.bit2025.mysite.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,15 +21,17 @@ import jakarta.servlet.ServletContext;
 public class AdminController {
 
 	private ServletContext servletContext;
+	private ApplicationContext applicationContext;
 
 	private FileuploadService fileuploadService;
 	private SiteService siteService;
 
-	public AdminController(ServletContext servletContext, SiteService siteService,
-			FileuploadService fileuploadService) {
+	public AdminController(ServletContext servletContext, ApplicationContext applicationContext,
+			SiteService siteService, FileuploadService fileuploadService) {
 		this.servletContext = servletContext;
 		this.siteService = siteService;
 		this.fileuploadService = fileuploadService;
+		this.applicationContext = applicationContext;
 	}
 
 	@RequestMapping({ "", "/" })
@@ -44,12 +48,18 @@ public class AdminController {
 		} else {
 			// 이미지 등록을 안 하면 이전 이미지 유지
 			siteVo.setProfileURL(siteService.getSite().getProfileURL());
-		}	
+		}
 		// form 입력 정보로 mysite 갱신
 		siteService.updateSite(siteVo);
-		// update SiteVo bean in application context
+
+		// update SiteVo bean in Servlet Context
 		servletContext.setAttribute("siteVo", siteVo);
 
+		// update SiteVo bean in Application Context
+		SiteVo site = applicationContext.getBean(SiteVo.class);
+		// siteVo -> site copy
+		BeanUtils.copyProperties(siteVo, site);
+		
 		return "redirect:/admin";
 	}
 
